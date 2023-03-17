2162f3e5facaf2825b63f4f8babe057806d4c70a
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-17 12:24:46 +0530

    Add files via upload

---
+++ gamedemo.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	int total;
+	
+		cout<<"enter 1st the number: ;
+		cin>>a;
+		cout<<"enter 2nd number: ;
+		cin>>b;
+		total=a+b;
+		cout << "sum is: " <<sum;
+	
+	
+
+getch();
+}
